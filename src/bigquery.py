8d8b216c4324b480a5262b8a82a7b5daa1b9db12
Armando <72764913+Armando284@users.noreply.github.com>
2025-01-27 01:14:04 -0500

    refactor: changes bigquery module so sql queries can be use to get data

--- src/bigquery.py
+++ src/bigquery.py
@@ -1,5 +1,5 @@
+import pandas as pd
 from google.cloud import bigquery
-import os
 
 def load_to_bigquery(csv_path: str, dataset_id: str, table_id: str) -> None:
     """
@@ -10,26 +10,32 @@ def load_to_bigquery(csv_path: str, dataset_id: str, table_id: str) -> None:
         dataset_id (str): ID of the BigQuery dataset.
         table_id (str): ID of the BigQuery table.
     """
-    # Configure BigQuery client
     client = bigquery.Client.from_service_account_json("credentials/tech-salary-insights-9a26b6273124.json")
-    
-    # Configure table ref
     table_ref = client.dataset(dataset_id).table(table_id)
-    
-    # Configure load job
     job_config = bigquery.LoadJobConfig()
     job_config.source_format = bigquery.SourceFormat.CSV
-    job_config.autodetect = True  # BigQuery will automatically detect schema
+    job_config.autodetect = True
     
-    # load CSV
     with open(csv_path, "rb") as source_file:
         job = client.load_table_from_file(source_file, table_ref, job_config=job_config)
     
-    job.result()  # wait job finish
-    print(f"Datos cargados en BigQuery: {dataset_id}.{table_id}")
+    job.result()
+    print(f"Data loaded to BigQuery: {dataset_id}.{table_id}")
 
-if __name__ == "__main__":
-    csv_path = "data/processed/cleaned_tech_salaries.csv"
-    dataset_id = "tech_salaries_dataset"  # Change by dataset ID
-    table_id = "cleaned_salaries"  # Change by table ID
-    load_to_bigquery(csv_path, dataset_id, table_id)
+def query_bigquery(sql_query: str) -> pd.DataFrame:
+    """
+    Run a SQL query on BigQuery and return results as a Pandas DataFrame.
+
+    Args:
+        sql_query (str): SQL query to execute.
+
+    Returns:
+        pd.DataFrame: Query results as a Pandas DataFrame.
+    """
+    client = bigquery.Client.from_service_account_json("credentials/tech-salary-insights-9a26b6273124.json")
+    query_job = client.query(sql_query)
+    results = query_job.result()
+
+    # Manual conversion to a DataFrame
+    rows = [dict(row) for row in results]
+    return pd.DataFrame(rows)
